403690644df789baa0caa4536bb6405d8c4e4eab
German Moyano <carlos.german.moyano@fisglobal.com>
2018-08-05 20:13:53 -0300

    adding nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { Routes, RouterModule } from '../../node_modules/@angular/router';
+import { AsadoManagerComponent } from './asado-manager/asado-manager.component';
+
+const appRouter: Routes = [
+  {path: 'asado-manager', component: AsadoManagerComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent,
+    AsadoManagerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    RouterModule.forRoot(appRouter)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/asado-manager/asado-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asado-manager',
+  templateUrl: './asado-manager.component.html',
+  styleUrls: ['./asado-manager.component.scss']
+})
+export class AsadoManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
